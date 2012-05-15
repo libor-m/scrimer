afaa6e55768fef4ad95bc2369645553706cab920
Libor Morkovsky <liborek@gmail.com>
2012-05-15 18:11:02 +0200

    design_primers.py alpha

--- design_primers.py
+++ design_primers.py
@@ -22,6 +22,7 @@
 #
 
 import sys
+import os
 import itertools
 import string
 
@@ -66,7 +67,7 @@ def find_pcr_primers(pseq, target):
     p3 = primer3_connector.Primer3(**def_params)
 
     rec_params = {
-    'SEQUENCE_ID':'design_primers_task',
+    'SEQUENCE_ID':'pick_pcr_primers_task',
     'SEQUENCE_TEMPLATE':pseq,
     'SEQUENCE_TARGET': "%d,1" % target,
     'PRIMER_OPT_SIZE':'19',
@@ -77,9 +78,61 @@ def find_pcr_primers(pseq, target):
     
     return p3.call([rec_params])
 
+# not used, documentation of primer3 is not clear enough
+# about how to use this feature..
+def find_gt_primers(pseq, target):
+    """call primer3 executable, return the results
+    """
+    def_params = {
+    'PRIMER_THERMODYNAMIC_PARAMETERS_PATH':'/opt/primer3/bin/primer3_config/',
+    'PRIMER_MAX_NS_ACCEPTED':'0',
+    }
+    
+    p3 = primer3_connector.Primer3(**def_params)
+
+    rec_params = {
+    'SEQUENCE_ID':'pick_gt_primers_task',
+    'SEQUENCE_TEMPLATE':pseq,
+    'SEQUENCE_TARGET': "%d,1" % target,
+    'PRIMER_TASK':'pick_discriminative_primers',
+    'SEQUENCE_INCLUDED_REGION':  "%d,1" % target,
+    'PRIMER_OPT_SIZE':'22',
+    'PRIMER_MIN_SIZE':'20',
+    'PRIMER_MAX_SIZE':'25',
+    'PRIMER_PRODUCT_SIZE_RANGE':'1-100',
+    }
+    
+    return p3.call([rec_params])
+
+def check_gt_primers(pseq, left_primer, right_primer_rc, **kwargs):
+    """call primer3 executable, return the results
+    """
+    def_params = {
+    'PRIMER_THERMODYNAMIC_PARAMETERS_PATH':'/opt/primer3/bin/primer3_config/',
+    'PRIMER_MAX_NS_ACCEPTED':'0',
+    'PRIMER_EXPLAIN_FLAG':'1', 
+    'PRIMER_PICK_ANYWAY':'1',
+    }
+    
+    def_params.update(kwargs)
+    
+    p3 = primer3_connector.Primer3(**def_params)
+
+    rec_params = {
+    'SEQUENCE_ID':'check_gt_primers_task',
+    'SEQUENCE_TEMPLATE': pseq,
+    'SEQUENCE_PRIMER': left_primer,
+    'SEQUENCE_PRIMER_REVCOMP': right_primer_rc,
+    'PRIMER_TASK':'check_primers',
+    'PRIMER_MIN_TM':'50',
+    'PRIMER_PRODUCT_SIZE_RANGE':'40-100',
+    }
+    
+    return p3.call([rec_params])
+
 #TODO: use some general score based on coverage, 
 # exon support etc in the gff score field
-def primer_to_gff(primer, tag, seq_start):
+def primer_to_gff(name, primer, tag, seq_start, strand):
     pos, len = map(int, primer['position'].split(','))
     
     # transfer the calculated values to attributes
@@ -89,9 +142,11 @@ def primer_to_gff(primer, tag, seq_start):
         if k == 'PENALTY' or k == 'position': continue
         at[k] = v
         
+    at['Name'] = name
+    
     gflist = [var.CHROM, 'design-primers', tag, 
         seq_start + pos, seq_start + pos + len,
-        primer['PENALTY'], '+', '.', str(at)]
+        primer['PENALTY'], strand, '.', str(at)]
         
     return pybedtools.create_interval_from_list(gflist)
 
@@ -122,11 +177,14 @@ def main():
         var_features = annotations.tabix_intervals(pybedtools.Interval(var.CHROM, var.POS, var.POS))
         var_exons = [f for f in var_features if f.fields[2] == 'exon']
         
+        if len(var_exons) == 0: continue
+        
         # get a minimal predicted exon
         # (trying to be conservative)
         # consider only predicted, not transferred exons (source in attrs)
-        ex_start = max(f.start for f in var_exons if 'source' in f.attrs)
-        ex_end = min(f.end for f in var_exons if 'source' in f.attrs)
+        ex_predicted = [f for f in var_exons if 'source' in f.attrs]
+        ex_start = max(f.start for f in ex_predicted)
+        ex_end = min(f.end for f in ex_predicted)
         min_exon = pybedtools.Interval(var.CHROM, ex_start, ex_end)
         
         # check if the exon has a sane size (> 70)
@@ -148,24 +206,49 @@ def main():
         
         # check if there is at least 20 fixed bases on either side
         # of the target variant because of the genotyping primer
-        gt_primers = get_flanking(pseq, var.POS, 25)
-        if all(map(lambda s: 'N' in s, gt_primers)): continue
+        gt_primer_seqs = get_flanking(pseq, var.POS - 1 - min_exon.start, 25)
+        # attach 'N' to end of each primer, so we get the full len
+        # instead of -1, when there is no N in the original primer
+        max_gt_lens = [(s + 'N').find('N') for s in  [gt_primer_seqs[0][::-1], gt_primer_seqs[1]]]
+        if all(x < 20 for x in max_gt_lens): continue
         
         # call primer3 to design PCR primers
         # mark only a single base - the variant of interest - as a target
         #TODO: call primer3 only once with multiple records?
         primers = find_pcr_primers(pseq, var.POS - 1 - min_exon.start)
         
+        # generate the name for the primer ensemble
+        # use the name from reference genome only if it is unique
+        # otherwise use the transcript name
+        ref_names = set(e.attrs['Name'] for e in var_exons if 'coords' in e.attrs)
+        if len(ref_names) == 1:
+            primer_name = ref_names.pop()
+        else:
+            primer_name = ex_predicted[0].attrs['Name']
+        
         # if something was found, output the first pair
         # (should be the best one)
         if len(primers[0]['PAIR']):
-            print primer_to_gff(primers[0]['LEFT'][0], 'primer-pcr-left')
-            print primer_to_gff(primers[0]['RIGHT'][0], 'primer-pcr-right')
+            print primer_to_gff('LU-PCR-%s-F' % primer_name, primers[0]['LEFT'][0], 'primer-pcr', '+')
+            print primer_to_gff('LU-PCR-%s-R' % primer_name, primers[0]['RIGHT'][0], 'primer-pcr', '-')
         else:
             continue
         
-        # if we've got PCR primers, try to check the genotyping ones
+        #TODO: if we've got PCR primers, try to check the genotyping ones
+        # BatchPrimer3 is checking 
+        # - melting temperature - GC content - repeats - Ns
+        # - self complementarity (by Smith-Waterman in perl)
+        # primer3 can be used to check GC, Tm, hairpins, self complementarity by thermodynamic approach
+        # that seems superior to perl-based approach
+        gt_primers = check_gt_primers(pseq, gt_primer_seqs[0][-max_gt_lens[0]:], reverse_complement(gt_primer_seqs[1][:max_gt_lens[1]]))
+        
+        # this should always report some results, so output those to gff
+        print primer_to_gff('LU-GT-%s-F' % primer_name, gt_primers[0]['LEFT'][0], 'primer-gt', '+')
+        print primer_to_gff('LU-GT-%s-R' % primer_name, gt_primers[0]['LEFT'][0], 'primer-gt', '-')
+        
+        #TODO: here should be some kind of scoring for the primer ensemble
+        # - number of other segregating polymorphisms in the same exon/?mRNA 
+        # - variant/primer coverage summary (would require .bam or some wiggle 
+        #   file)
 
 if __name__ == "__main__": main()
-
-
