0954d29045b7b101f4389235fca6a5f6a17019ec
Libor Morkovsky <liborek@gmail.com>
2012-10-10 01:34:30 +0200

    added prepare script

---
+++ 0-prepare.sh
@@ -0,0 +1,50 @@
+#------------------------------------------------
+# Operation
+#------------------------------------------------
+
+#  1. downlaod and prepare the reference genome
+#  2. downlaod all needed annotations
+#  3. and build genome indexes for all programs used in the pipeline
+#  4. quality check of the input data
+
+# 1. reference genome
+#------------------------------------------------
+
+# database list at http://hgdownload.cse.ucsc.edu/downloads.html#zebrafinch
+# download full data set, interrupt during xenoMrna (not needed, too big)
+# command displayed in the file listing
+
+# location of genome data that can be sharad among users
+cd /data/genomes
+GENOME=taeGut1
+mkdir $GENOME
+cd $GENOME
+rsync -avzP rsync://hgdownload.cse.ucsc.edu/goldenPath/$GENOME/bigZips/ .
+
+# check downloaded data integrity
+md5sum -c md5sum.txt
+cat *.md5|md5sum -c
+
+# unpack the genome - this differs for genomes
+# some are in single .fa, some are split by chromosomes
+tar xvzf chromFa.tar.gz
+
+# 2. annotataions
+#------------------------------------------------
+# annotation data is best obtained in UCSC table browser
+# in BED format and then sorted and indexed by BEDtools
+# http://genome.ucsc.edu/cgi-bin/hgTables?db=taeGut1
+ANNOT=annot
+sortBed -i $ANNOT/ensGene.bed > $ANNOT/ensGene.sorted.bed
+bgzip $ANNOT/ensGene.sorted.bed
+tabix -p bed $ANNOT/ensGene.sorted.bed.gz
+
+# 
+# 3. indexes
+#------------------------------------------------
+# to be done
+
+# 4. quality check
+#------------------------------------------------
+mkdir 01-fastqc
+fastqc --outdir=01-fastqc --noextract --threads=8 00-raw/*.fastq
